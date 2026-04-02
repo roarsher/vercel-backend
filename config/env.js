fec8752a2693303593e87f97b8e54a90011ab28a
roarsher <154459620+roarsher@users.noreply.github.com>
2026-04-03 00:47:22 +0530

    change

--- config/env.js
+++ config/env.js
@@ -3,6 +3,7 @@ module.exports = {
   MONGO_URI:   process.env.MONGO_URI,
   JWT_SECRET:  process.env.JWT_SECRET,
   JWT_EXPIRE:  process.env.JWT_EXPIRE  || '7d',
-  CLIENT_URL:  process.env.CLIENT_URL  || 'http://localhost:3000',
+  // CLIENT_URL:  process.env.CLIENT_URL  || 'http://localhost:3000',
+  CLIENT_URL:  process.env.CLIENT_URL  || 'https://vercel-backend-1-ixpf.onrender.com',
   NODE_ENV:    process.env.NODE_ENV    || 'development',
 };
--- server.js
+++ server.js
@@ -12,9 +12,12 @@ connectDB();
 const app = express();
 
 // ── MIDDLEWARE ──────────────────────────────────────
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true,
+ app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://fullfrontend-nine.vercel.app"  // ✅ ADD THIS
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
